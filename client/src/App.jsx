c7ae8e6ac99a99186bc470c6b7f4927ef6407948
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-17 03:30:35 +0530

    apollo set-up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+  // useQuery,
+  // gql
+} from "@apollo/client";
+
+//components :
 import BookList from "./components/BookList";
+
+//Apollo client setup :
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <div id="main">
-      <h1>GraphQL Library</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>GraphQL Library</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
